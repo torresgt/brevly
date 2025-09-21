37104f26a51458d6b7d4c5e59f43da27af7a5d62
Gabriel <gabriel_torres2711@hotmail.com>
2025-09-21 15:45:33 -0300

    refactor: Finished csv exportation request

--- server/src/app/functions/export-links.ts
+++ server/src/app/functions/export-links.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from "node:crypto";
 
 import { PutObjectCommand } from "@aws-sdk/client-s3";
+import dayjs from "dayjs";
 import Papa from "papaparse";
 import { env } from "@/env";
 import { db } from "@/infra/db";
@@ -28,38 +29,45 @@ export async function exportLinks(): Promise<
 			return makeLeft(new ExportError("Não existem links para exportar."));
 		}
 
-		// Mapeia os dados para o formato do CSV, definindo os cabeçalhos
-		const csvData = links.map((link) => ({
-			"URL Original": link.originalUrl,
-			"URL Encurtada": link.shortener,
-			"Contagem de Acessos": link.accessCount,
-			"Data de Criação": link.createdAt.toISOString(),
-		}));
+		const csvString = Papa.unparse(
+			{
+				fields: [
+					"URL Original",
+					"URL Encurtada",
+					"Contagem de Acesso",
+					"Data de Criação",
+				],
+				data: links.map((link) => [
+					link.originalUrl,
+					link.shortener,
+					link.accessCount,
+					dayjs(link.createdAt).format("DD/MM/YY - HH:mm"),
+				]),
+			},
+			{ delimiter: ";" },
+		);
 
-		// Converte o array de objetos para uma string no formato CSV
-		const csvString = Papa.unparse(csvData);
+		const fileName = `brevly-links${randomUUID()}.csv`;
+		const csvStringBOM = `\uFEFF${csvString}`; // To treat special characters in csv file
 
-		// Gera um nome de arquivo aleatório e único
-		const fileName = `${randomUUID()}.csv`;
-
-		// Envia o arquivo para o bucket do Cloudflare R2
 		await r2.send(
 			new PutObjectCommand({
 				Bucket: env.CLOUDFLARE_BUCKET,
 				Key: fileName,
-				Body: csvString,
-				ContentType: "text/csv",
+				Body: csvStringBOM,
+				ContentType: "text/csv;charset=utf-8",
 			}),
 		);
 
-		// Constrói a URL pública final do arquivo
 		const publicUrl = new URL(fileName, env.CLOUDFLARE_PUBLIC_URL).toString();
 
 		return makeRight({ exportUrl: publicUrl });
 	} catch (error) {
-		console.error("Falha ao exportar os links:", error);
+		console.error("Error to export links:", error);
 		return makeLeft(
-			new ExportError("Ocorreu um erro inesperado ao exportar os links."),
+			new ExportError(
+				"An unexpected error occurred while exporting the links.",
+			),
 		);
 	}
 }
