8dd60ea5caa43b0702fa80cc73ddbdb0ec23a5e5
Gabriel <gabriel_torres2711@hotmail.com>
2025-09-21 15:44:27 -0300

    refactor: New validations for create link requisition

--- server/src/infra/http/routes/create-link.ts
+++ server/src/infra/http/routes/create-link.ts
@@ -1,25 +1,48 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { createLink } from "@/app/functions/create-link";
+import { isLeft } from "@/shared/either";
+
 export const createLinkRoute: FastifyPluginAsyncZod = async (server) => {
 	server.post(
 		"/urls",
 		{
 			schema: {
 				summary: "create a link",
+				tags: ["links"],
 				body: z.object({
-					name: z.string(),
-					password: z.string().optional(),
+					url: z.url({ message: "Please, provide a valid URL" }),
+					shortener: z
+						.url({ message: "URL invalid" })
+						.regex(/^[a-z0-9_-]+$/, { message: "URL shortener is malformed." })
+						.min(3, {
+							message: "The shortener must have at least 3 characters",
+						})
+						.transform((val) => val.toLowerCase())
+						.optional(),
 				}),
 				response: {
-					200: z.object({ urlId: z.string() }),
-					400: z
+					201: z.object({
+						linkId: z.uuid(),
+					}),
+					409: z
 						.object({ message: z.string() })
-						.describe("URL link already exists."),
+						.describe("Conflict: URL link already exists."),
 				},
 			},
 		},
-		async (_request, reply) => {
-			return reply.status(200).send({ urlId: "teste" });
+		async (request, reply) => {
+			const { url, shortener } = request.body;
+
+			const result = await createLink({ url, shortener });
+
+			if (isLeft(result)) {
+				const { message } = result.left;
+				return reply.status(409).send({ message });
+			}
+
+			const { linkId } = result.right;
+			return reply.status(201).send({ linkId });
 		},
 	);
 };
