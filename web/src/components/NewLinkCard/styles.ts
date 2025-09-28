ffcc8cd7abdb85164f3e90351f13468b9ac8f335
Gabriel <gabriel_torres2711@hotmail.com>
2025-09-28 20:44:59 -0300

    refactor: create link component

--- web/src/components/NewLinkCard/styles.ts
+++ web/src/components/NewLinkCard/styles.ts
@@ -1,25 +1,26 @@
-import { Button, Card } from "antd";
+import { CheckCircleOutlined } from "@ant-design/icons";
+import { Button, Card, Input } from "antd";
 import { rem } from "polished";
 import styled from "styled-components";
 
 export const CardContainer = styled(Card)`
   width: 100%;
-  min-height: ${rem(340)};
-  max-width: ${rem(440)};
+  min-height: ${rem(360)};
+  min-width: ${rem(350)};
   border-radius: 8px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.09);
   border: none; 
 
   .ant-card-body {
-    padding: 24px 32px; 
+    padding: ${rem(24)} ${rem(32)}; 
   }
 
   .ant-form-item {
-    margin-bottom: 24px;
+    margin-bottom: ${rem(24)};
   }
  
   .ant-form-item-label > label {
-    font-size: 12px;
+    font-size: ${rem(12)};
     font-weight: 600;
     color: ${({ theme }) => theme.colors.text};
   }
@@ -61,4 +62,34 @@ export const StyledButton = styled(Button)`
     background-color: #7A8CFE !important;
     border-color: #7A8CFE !important;
   }
+  &:disabled:hover {
+    background-color: #f5f5f5 !important;
+    border-color: #d9d9d9 !important;
+  }
+`;
+
+export const ShortenerInput = styled(Input)`
+  height: ${rem(40)} !important;
+  .ant-input-outlined, .ant-input-prefix { 
+    color: rgba(0, 0, 0, 0.25);
+    font-weight: 400;
+  }
+`;
+
+export const ToastContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
+
+export const CircleOutlined = styled(CheckCircleOutlined)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: #fff;
+  width: ${rem(14)};
+  height: ${rem(14)};
+  border-radius: 99%;
+  background-color: #80c343;
 `;
